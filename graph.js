bbc4507c268f3a7859a1598406ae802cc20c6136
theUyonator <duyo@ualberta.ca>
2021-09-19 15:22:26 -0600

    Methods added to graph class

--- graph.js
+++ graph.js
@@ -11,25 +11,77 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.addVertex(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    this.nodes.delete(vertex);
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const toVisitStack = [start];
+    const seen = new Set(toVisitStack);
+    const result = [];
+    let current;
+
+  while (toVisitStack.length > 0) {
+    current = toVisitStack.pop();
+    result.push(current.value);
+
+    for (let neighbor of current.adjacent) {
+      if (!seen.has(neighbor)) {
+        toVisitStack.push(neighbor);
+        seen.add(neighbor);
+      }
+    }
+  }
+
+  return result;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    const toVisitStack = [start];
+    const seen = new Set(toVisitStack);
+    const result = [];
+    let current;
+
+  while (toVisitStack.length > 0) {
+    current = toVisitStack.shift();
+    result.push(current.value);
+
+    for (let neighbor of current.adjacent) {
+      if (!seen.has(neighbor)) {
+        toVisitStack.push(neighbor);
+        seen.add(neighbor);
+      }
+    }
+  }
+
+  return result;
+  }
 }
 
 module.exports = {Graph, Node}
